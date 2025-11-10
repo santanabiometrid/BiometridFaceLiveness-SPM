89c4ad3e4833bd9aaad1a2a90f181b93bbed0147
André Santana <andre.santana@biometrid.com>
2025-11-10 11:12:23 +0000

    add correct package

--- Package.swift
+++ Package.swift
@@ -7,21 +7,20 @@ let package = Package(
     name: "BiometridFaceLibrary",
     platforms: [
         .iOS(.v15)
-    ]
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BiometridFaceLibrary",
-            targets: ["FaceTecSDK"]),
+            targets: ["FaceTecSDK"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            .binaryTarget(name: "FaceTecSDK",
-                          url: "https://dl.cloudsmith.io/public/biometrid/mobile-test/raw/versions/1.0.0/FaceTecSDK.framework.zip",
-                          checksum: "cffbd6c3ef54c0e312955d8ba05f96feac7d3eb5b6d36442a09b6607c1e76f84"
-                         )
+        // Binary target declaration — this should NOT be wrapped inside `.target()`.
+        .binaryTarget(
+            name: "FaceTecSDK",
+            url: "https://dl.cloudsmith.io/public/biometrid/mobile-test/raw/names/BiometridFaceLiveness/versions/1.0.0/FaceTecSDK.xcframework.zip",
+            checksum: "cffbd6c3ef54c0e312955d8ba05f96feac7d3eb5b6d36442a09b6607c1e76f84"
         )
     ]
 )
