f48928b1930827e6752ff7cdb1bcade9d770595a
André Santana <andre.santana@biometrid.com>
2025-11-10 11:42:12 +0000

    change package

--- Package.swift
+++ Package.swift
@@ -3,11 +3,6 @@
 
 import PackageDescription
 
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
 let package = Package(
     name: "BiometridFaceLiveness",
     platforms: [
@@ -16,18 +11,10 @@ let package = Package(
     products: [
         .library(
             name: "BiometridFaceLiveness",
-            targets: ["BiometridFaceLibraryTarget"]
+            targets: ["FaceTecSDK"]
         ),
     ],
     targets: [
-        // Wrapper target that re-exports the binary SDK
-        .target(
-            name: "BiometridFaceLibraryTarget",
-            dependencies: ["FaceTecSDK"],
-            path: "Sources/BiometridFaceLibraryTarget"
-        ),
-
-        // Binary target
         .binaryTarget(
             name: "FaceTecSDK",
             url: "https://dl.cloudsmith.io/public/biometrid/mobile-test/raw/versions/1.0.0/FaceTecSDK.xcframework.zip",
@@ -35,4 +22,3 @@ let package = Package(
         )
     ]
 )
-
